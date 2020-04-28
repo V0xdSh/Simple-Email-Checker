dbf0f52b7e003628948f10218f2dce319fa474a3
SizeDevv <43902768+SizeDevv@users.noreply.github.com>
2020-04-28 22:31:45 +0200

    Create Email_Checker.py

---
+++ Email_Checker.py
@@ -0,0 +1,20 @@
+import json
+from urllib.request import urlopen
+
+'''
+Simple Email Checker
+'''
+
+email_checker = input("Email : ")
+
+key = "BSnzI5Z8LFU7EVmyc9hARGTw2JvPKC0j"
+
+url = "https://emailverifierapi.com/v2/?apiKey=" + key + "&email=" + email_checker
+response = urlopen(url)
+data = json.load(response)
+details=data['details']
+
+if details == "The mailbox exists.":
+    print("Email Exist !")
+else:
+    print("nope!")
