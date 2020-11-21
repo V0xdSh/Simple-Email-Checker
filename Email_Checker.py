93b0c4998b18eb80a041b93ed2eb7e0439a432df
JeanLeKhey <43902768+JeanLeKhey@users.noreply.github.com>
2020-11-21 10:30:16 +0100

    Update Email_Checker.py

--- Email_Checker.py
+++ Email_Checker.py
@@ -7,7 +7,7 @@ Simple Email Checker
 
 email = input("Email : ")
 
-key = "YdJMzmBP6hkHxQsWcjwbZT8nLf7rVFUq"
+key = "IhuPynxZSqjfvX8T0e2cFkAa65gYVGMK"
 
 url = "https://emailverifierapi.com/v2/?apiKey=" + key + "&email=" + email
 
